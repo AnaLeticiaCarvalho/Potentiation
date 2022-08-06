70c384722407544c4e40ccdafe3a8d80789b4532
Ana Leticia Carvalho <analeticiacarvalho01@gmail.com>
2022-08-06 15:41:14 -0300

    Pontentiation calculator

---
+++ potentiation.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    float base;
+    cout << "Digite o número: ";
+    cin >> base;
+    printf("%f",pow(base,2));
+    printf("%f",pow(base,3));
+
+}
